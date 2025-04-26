90c262fe1c19152fee9cf8eb48158da5b7dee1ec
aachan1127 <aachan1127y@gmail.com>
2025-04-26 23:19:02 +0900

    udemy16 reactive関数

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <script setup>
-import { ref } from 'vue' // ref 関数を呼び出すことで、priceのデータが再レンダリングされる
+import { ref, reactive } from 'vue'
+// ref 関数を呼び出すことで、priceのデータが再レンダリングされる
+// reactive 関数 元々オブジェクトになっているやつはreactive関数を使うことで、いちいちオブジェクトを作らなくていい（ここでいうinstructor）
 
 const title = ref('Vue.js Course')
 let price = ref(9.99) // refオブジェクト
+
 function increment() {
   price.value += 1 // .valueをつけることで、 priceの値が再レンダリングされる
-  // console.log(price.value)
+  instructor.age += 1 // reactive関数で作られているので、 .valueつけなくても再レンダリングされる
+  instructor.bio = 'hi'
+  instructor.sns.twitter = 'hello'
 }
+
 const info = ref({
   student: 1000,
   rating: 4,
 })
-console.log(info.value.student)
+
+const instructor = reactive({
+  name: 'Akane',
+  age: 33,
+  sns: {
+    //reactive関数の中のオブジェクトは、reactiveになる！
+    twitter: '@aachan',
+    youtube: '@Akane',
+  },
+})
+instructor.bio = 'hello'
+console.log(info.value.student) //ref関数なので、.valueをつける
+console.log(instructor.sns) //reactive関数なので、.valueつけなくてもいい
 </script>
 
 <template>
@@ -19,6 +37,9 @@ console.log(info.value.student)
   <h2>Price: {{ price }}</h2>
   <button @click="increment">button</button>
   <h2>Students: {{ info.student }}</h2>
+  <h2>Instructor age: {{ instructor.age }}</h2>
+  <h2>Instructor bio: {{ instructor.bio }}</h2>
+  <h2>Instructor SNS Twitter: {{ instructor.sns.twitter }}</h2>
 </template>
 
 <style>
